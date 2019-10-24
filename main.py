0c7fba3e9c9685ef3ff9e04921ecb8be384820b9
Evgeniy Isakov <jellywillofficial@gmail.com>
2019-10-24 22:50:40 +0300

    Version 1.0

---
+++ main.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen, urlretrieve
+import re
+import os
+
+def download_files(link, regular_link, regular_name):
+    resp = urlopen(link)
+    htmltext=resp.read().decode('utf-8')
+
+    titles = re.findall(regular_link, htmltext)
+    for title in titles:
+        try:
+            urlretrieve('https://2ch.hk' + title, 'Download\\' + re.findall(regular_name, title)[0])
+        except Exception as e:
+            print(e)
+
+
+if not os.path.exists('Download'):
+    os.makedirs('Download')
+
+case = input('Input 1 for pictures, input 2 for videos: ')
+site = input('Input link thread: ')
+
+if case is '1':
+    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.jpg', '\d{1,}.jpg')
+    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.png', '\d{1,}.png')
+elif case is '2':
+    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.webm', '\d{1,}.webm')
+    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.mp4', '\d{1,}.mp4')
