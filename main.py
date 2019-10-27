601630ad0aac8b47372f8e6258ca344a67c59cb7
Evgeniy Isakov <jellywillofficial@gmail.com>
2019-10-27 13:57:27 +0300

    Version 1.1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def download_files(link, regular_link, regular_name):
     titles = re.findall(regular_link, htmltext)
     for title in titles:
         try:
-            urlretrieve('https://2ch.hk' + title, 'Download\\' + re.findall(regular_name, title)[0])
+            urlretrieve('https://2ch.hk' + title, os.path.join('Download', re.findall(regular_name, title)[0]))
         except Exception as e:
             print(e)
 
@@ -21,8 +21,8 @@ case = input('Input 1 for pictures, input 2 for videos: ')
 site = input('Input link thread: ')
 
 if case is '1':
-    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.jpg', '\d{1,}.jpg')
-    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.png', '\d{1,}.png')
+    download_files(site, '/b\/src\/\d{1,}\/\w{1,}.jpg', '\w{1,}.jpg')
+    download_files(site, '/b\/src\/\d{1,}\/\w{1,}.png', '\w{1,}.png')
 elif case is '2':
-    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.webm', '\d{1,}.webm')
-    download_files(site, '/b\/src\/\d{1,}\/\d{1,}.mp4', '\d{1,}.mp4')
+    download_files(site, '/b\/src\/\d{1,}\/\w{1,}.webm', '\w{1,}.webm')
+    download_files(site, '/b\/src\/\d{1,}\/\w{1,}.mp4', '\w{1,}.mp4')
